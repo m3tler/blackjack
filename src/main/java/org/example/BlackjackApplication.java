48cd31b91185d4353a63ee516480166b29746de1
m3tler <dawid.metlerski@outlook.com>
2022-10-26 17:15:19 +0200

    Create ApplicationManager

--- src/main/java/org/example/BlackjackApplication.java
+++ src/main/java/org/example/BlackjackApplication.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.app.ApplicationManager;
+
 public class BlackjackApplication {
     public static void main(String[] args) {
-
+        ApplicationManager applicationManager = new ApplicationManager();
+        applicationManager.run();
     }
 }
---
+++ src/main/java/org/example/app/ApplicationManager.java
@@ -0,0 +1,7 @@
+package org.example.app;
+
+public class ApplicationManager {
+    public void run() {
+
+    }
+}
