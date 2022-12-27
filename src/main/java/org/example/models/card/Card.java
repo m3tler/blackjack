9f1b07b970c9659a2b87f38a00dd24197d6d1dd5
m3tler <dawid.metlerski@outlook.com>
2022-12-27 18:47:04 +0100

    Convert the Card class into a record

--- src/main/java/org/example/models/card/Card.java
+++ src/main/java/org/example/models/card/Card.java
@@ -1,22 +1,6 @@
 package org.example.models.card;
 
-public class Card {
-    private final CardValue cardValue;
-    private final CardSuit cardSuit;
-
-    public Card(CardValue cardValue, CardSuit cardSuit) {
-        this.cardValue = cardValue;
-        this.cardSuit = cardSuit;
-    }
-
-    public CardValue getCardValue() {
-        return cardValue;
-    }
-
-    public CardSuit getCardSuit() {
-        return cardSuit;
-    }
-
+public record Card(CardValue cardValue, CardSuit cardSuit) {
     @Override
     public String toString() {
         return cardValue + " of " + cardSuit + "s";
